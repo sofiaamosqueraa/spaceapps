444953d8bc58176edef0a637925c0ab084fd7dea
sofiaamosqueraa <146948982+sofiaamosqueraa@users.noreply.github.com>
2023-10-08 12:23:03 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+function showvideo(){
+    var video = document.getElementById("live-video");
+    var map = document.getElementById("live-map");
+    var container = document.getElementById("video-container");
+
+    if  (video.style.display === "none") {
+        container.style.height = "520px"
+    } else {
+        container.style.height = "1px";
+    }
+    if  (video.style.display === "none") {
+        video.style.display = "block"
+    } else {
+        video.style.display = "none";
+    }
+    if  (map.style.display === "none") {
+        map.style.display = "flex"
+    } else {
+        map.style.display = "none";
+    }
+   
+}
